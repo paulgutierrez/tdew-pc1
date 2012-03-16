87f13b910506d6a30e7531fd45450e8bd2b6ef8e
Paul Gutierrez <slash4ever@gmail.com>
2012-03-15 21:36:18 -0500

    modificado en la pc

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,3 @@
 class Usuario < ActiveRecord::Base
+	has_many :rols
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u1=Usuario.new("paul","gutierrez","magallanes","12/05/1984")
+u2=Usuario.new("claudia","peña","herrera","03/03/1988")
+u3=Usuario.new("nikolaviech","solovieski","gutierrez","17/06/2010")
+u4=Usuario.new("fernanda","de las casas","maldini","15/07/1989")
+
+r1=Rol.new("u1","supervisor")
+r2=Rol.new("u1","delegado")
+r3=Rol.new("u2","psicologo")
+r4=Rol.new("u2","auxiliar")
+r5=Rol.new("u3","actor")
+r6=Rol.new("u4","actriz")
